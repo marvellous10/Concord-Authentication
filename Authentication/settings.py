bf581e51942146ef3ef5b7745be6a0b2c6bf8a7c
marvellous10 <nwachukwumarvelous90@gmail.com>
2024-05-01 13:41:44 +0100

    allowed hosts

--- Authentication/settings.py
+++ Authentication/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['concord-authentication.onrender.com']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
