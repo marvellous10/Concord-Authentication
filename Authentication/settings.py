edc7cfb688291d9b825d7e4ccb379be20b4ac7f4
marvellous10 <nwachukwumarvelous90@gmail.com>
2024-06-07 12:33:54 +0100

    disable debug

--- Authentication/settings.py
+++ Authentication/settings.py
@@ -14,9 +14,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST'), '127.0.0.1']
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST')]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
